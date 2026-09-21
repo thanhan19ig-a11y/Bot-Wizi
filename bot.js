ad5e9881caf553f565f53af29e7dab99332b0ea8
thanhan19ig-a11y <thanhan19.ig@gmail.com>
2026-09-21 20:45:54 +0700

    Add files via upload

--- bot.js
+++ bot.js
@@ -42,8 +42,8 @@ client.on("guildMemberAdd", async (member) => {
       .setColor(0x5eead4)
       .setTitle("Ch\u00e0o m\u1eebng " + name + " \u0111\u1ebfn v\u1EDBi " + server + "!")
       .setThumbnail(member.user.displayAvatarURL())
-      .addFields({ name: "Th\u00e0nh vi\u00ean th\u1ee9", value: String(member.guild.memberCount), inline: true })
-      .setFooter({ text: "Ng\u00e0y v\u00e0o: " + joinDate });
+      .setDescription("🎈 Th\u00e0nh vi\u00ean th\u1ee9 " + member.guild.memberCount + "\n📅 Ng\u00e0y v\u00e0o: " + joinDate + "\n💬 L\u1EDDi nh\u1EAFn: Ch\u00fac b\u1EA1n c\u00f3 tr\u1EA3i nghi\u1EC7m tuy\u1EC7t v\u1EDBi t\u1EA1i server!")
+      .setFooter({ text: "Wizi • " + server });
     await ch.send({ embeds: [emb] });
     console.log("welcomed:", name);
   } catch (e) {
